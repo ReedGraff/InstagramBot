91b82ccb6ddb07d8d632c024576ce3801f6c746f
Reed Graff <71910763+ReedGraff@users.noreply.github.com>
2021-06-27 16:12:55 -0500

    Update Index.py

--- Bot_Example/Index.py
+++ Bot_Example/Index.py
@@ -6,4 +6,4 @@ Bot = Base.Bot("YOURUSERNAME", "YOURPASSWORD")
 
 Bot.log()
 Bot.DIY_txt_to_list("Assets/Follow_File.txt")
-Bot.DIY_follow_from_list_with_selenium()
+Bot.DIY_follow_from_list_with_selenium() # You can also use: Bot.DIY_follow_from_list()
