ca83ad5cd40e7d4e3564d7045545156cf02f548d
Reed Graff <71910763+ReedGraff@users.noreply.github.com>
2021-06-27 16:11:20 -0500

    Update Index.py

--- Bot_Example/Index.py
+++ Bot_Example/Index.py
@@ -5,5 +5,5 @@ import Base
 Bot = Base.Bot("YOURUSERNAME", "YOURPASSWORD")
 
 Bot.log()
-Bot.DIY_txt_to_list("Assets/Compeers.txt")
+Bot.DIY_txt_to_list("Assets/Follow_File.txt")
 Bot.DIY_follow_from_list_with_selenium()
